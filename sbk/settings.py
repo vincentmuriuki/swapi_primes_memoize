3d1a298e21c4d0b71bb35850120212a24681321b
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-03-12 16:28:04 +0300

    Added django heroku settings

--- sbk/settings.py
+++ sbk/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
