96fcf1e3ef93665eaac266260eadb16e8fb0efdd
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-03-12 08:23:15 +0300

    Function  that implements binary search algirithm to find a value in a collection

---
+++ search.py
@@ -0,0 +1,18 @@
+def search(people_arr, x):
+    people_arr = sorted(people_arr)
+    start = 0
+    arr_len = len(people_arr) - 1
+    while start <= arr_len:
+        mid = start + int((arr_len - start)/2)
+        if people_arr[mid] == x:
+            return mid
+        elif people_arr[mid] < x:
+            start = mid + 1
+        else:
+            arr_len = mid - 1
+    return -1
+
+sample_arr = [2, 4, 6, 3, 8]
+value = 4
+res = search(sample_arr, value)
+print(res)
