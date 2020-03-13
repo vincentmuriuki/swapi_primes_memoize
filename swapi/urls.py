6f07b8381cbd532498c5e2fad5b38d436c0feaf0
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-03-13 13:52:39 +0300

    Added link to view single character details

--- swapi/urls.py
+++ swapi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import GetFavChar, GetChar
 
+# app_name = 'view'
 urlpatterns = [
-    path('', GetFavChar.as_view(template_name='index.html'), name='CharacterView'),
-    path('details/', GetChar.as_view(template_name='single_char.html'), name='UserView')
+    path('', GetFavChar.as_view(template_name='index.html'), name='view-all'),
+    path('details/', GetChar.as_view(template_name='single_char.html'), name='view-one')
 ]
